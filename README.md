69b241d8df5e7b96f98d7eaf1945b6b3ec454ac5
meir@redislabs.com <meir@redislabs.com>
2020-04-15 22:54:58 +0300

    first commit, added client code.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # GearsCli
 Simple cli that allows the send python code the RedisGears
+
+## Install
+```python
+pip install git+https://github.com/RedisGears/GearsCli.git
+```
+
+## Usage
+```
+usage: gears-cli [-h] [--host HOST] [--port PORT]
+                 [--requirements REQUIREMENTS] [--password PASSWORD]
+                 path [extra_args [extra_args ...]]
+
+Run gears scripts on Redis(Gears)
+
+positional arguments:
+  path                  scripts paths
+  extra_args            extra argument to send with the script (default: [])
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --host HOST           redis host (default: localhost)
+  --port PORT           redis port (default: 6379)
+  --requirements REQUIREMENTS
+                        requirements file (default: None)
+  --password PASSWORD   redis password (default: None)
+```
---
+++ gears_cli/__main__.py
@@ -0,0 +1,78 @@
+import redis
+import argparse
+import json
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description='Run gears scripts on Redis(Gears)')
+
+parser.add_argument(
+    '--host', default='localhost',
+    help='redis host')
+
+parser.add_argument(
+    '--port', default=6379, type=int,
+    help='redis port')
+
+parser.add_argument(
+    '--requirements', default=None,
+    help='requirements file')
+
+parser.add_argument('path', help='scripts paths')
+parser.add_argument('extra_args', help='extra argument to send with the script', nargs='*', default=[])
+
+parser.add_argument(
+    '--password', default=None,
+    help='redis password')
+
+args = parser.parse_args()
+
+def PP(res, resId):
+	res = str(res)
+    try:
+        res = json.loads(res)
+        print('%d) json:' % resId)
+        print(json.dumps(res, indent=4, sort_keys=True))
+        print('')
+        return
+    except Exception as e:
+        pass
+    print('%d)\t%s' % (resId, res))
+
+def main():
+	try:
+	    r = redis.Redis(args.host, args.port, password=args.password, decode_responses=True)
+	    r.ping()
+	except:
+	    print('Cannot connect to Redis. Aborting.')
+	    exit(1)
+
+	if args.requirements is not None:
+	    args.extra_args.append('REQUIREMENTS')
+	    with open(args.requirements, 'r') as f:
+	        requirements = [(el.strip()) for el in f.readlines()] 
+	        args.extra_args += requirements
+
+	with open(args.path, 'rt') as f:
+		script = f.read()
+	q = ['rg.pyexecute', script] + args.extra_args
+
+	reply = r.execute_command(*q)
+	
+	if reply == 'OK':
+	    print('OK')
+	else:
+	    results, errors = reply
+	    print('Results')
+	    print('-------')
+	    for i in range(len(results)):
+	    	PP(results[i], i + 1)
+	    print('')
+	    if len(errors) > 0:
+		    print('Errors')
+		    print('------')
+		    for i in range(len(errors)):
+		        print('%d)\t%s', (i + 1, str(errors[i])))
+
+
+if __name__ == '__main__':
+    main()
