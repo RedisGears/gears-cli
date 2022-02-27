779325923af4c3b28261f38821a3348a7989d8cb
meir <meir@redis.com>
2022-02-27 15:47:06 +0200

    added tls support

--- pytests/test_basics.py
+++ pytests/test_basics.py
@@ -5,23 +5,25 @@ import os
 
 Defaults.decode_responses = True
 
+SSL_ARGS = ['--ssl-keyfile', './tests/tls/redis.key', '--ssl-certfile', './tests/tls/redis.crt']
+
 def set_acl_user(env):
     env.expect('ACL', 'SETUSER', 'foo', 'on', '>pass', '+@all').ok()
 
 def run_internal_test(env, extra_args=[]):
     runner = CliRunner()
-    result = runner.invoke(run, extra_args + ['./pytests/example.py'])
+    result = runner.invoke(run, extra_args + SSL_ARGS + ['./pytests/example.py'])
     env.assertEqual(result.exit_code, 0)
     env.assertContains('"1"', result.output)
 
 def requirements_internal_test(env, extra_args=[], after_restart_callback=None):
     runner = CliRunner()
-    result = runner.invoke(install_requirements, extra_args + ['redis'])
+    result = runner.invoke(install_requirements, extra_args + SSL_ARGS + ['redis'])
     env.assertEqual(result.exit_code, 0)
     env.assertContains('Done', result.output)
 
     # export requirement redis
-    result = runner.invoke(export_requirements, extra_args + ['--requirement', 'redis'])
+    result = runner.invoke(export_requirements, extra_args + SSL_ARGS + ['--requirement', 'redis'])
     env.assertEqual(result.exit_code, 0)
     env.assertContains('Saving exported requirement into ', result.output)
     exported_file_path = result.output[result.output.find('/') : result.output.find('zip') + 3]
@@ -32,7 +34,7 @@ def requirements_internal_test(env, extra_args=[], after_restart_callback=None):
         after_restart_callback(env)
 
     # import redis requirement
-    result = runner.invoke(import_requirements, extra_args + ['--requirements-path', os.path.dirname(exported_file_path), os.path.basename(exported_file_path)])
+    result = runner.invoke(import_requirements, extra_args + SSL_ARGS + ['--requirements-path', os.path.dirname(exported_file_path), os.path.basename(exported_file_path)])
     env.assertEqual(result.exit_code, 0)
     env.assertContains('imported successfully', result.output)
 
